c634c60e979e97e9beaa27be543fb4c889af5549
IvanCNT <ivan.toma@gmail.com>
2025-09-06 00:27:26 -0300

    1

---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,16 @@
+let a=1;
+let b=2;
+
+//encontre 5 expressões true
+console.log(a<b);
+console.log(a<=b);
+console.log(b>a);
+console.log(b>=a);
+console.log(a!=b);
+
+//encontre 5 expressões false
+console.log(a>b);
+console.log(a>=b);
+console.log(b<a);
+console.log(b<=a);
+console.log(a==b);
